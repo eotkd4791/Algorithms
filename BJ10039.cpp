8f26bd43c1e82d1c93c827d500fd6645783870a5
eotkd4791 <eotkd4791@gmail.com>
2019-01-10 20:00:55 +0900

    bj10039

---
+++ BJ10039.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int arr[5];
+
+int main() 
+{
+		int result = 0;
+
+		for (int i = 0; i < 5; i++) {
+			scanf("%d", &arr[i]);
+		}
+		for (int i = 0; i < 5; i++) {
+			if (arr[i] <= 40)
+				arr[i] = 40;
+			result += arr[i];
+		}
+		printf("%d\n", result / 5);
+	
+	return 0;
+}
