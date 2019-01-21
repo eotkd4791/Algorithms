5921e36a09315862a9d4a45ae57fb65698b3f3de
eotkd4791 <eotkd4791@gmail.com>
2019-01-21 16:42:37 +0900

    update

---
+++ Math/BJ10817.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int a, b, c;
+
+int main() {
+
+	scanf_s("%d %d %d", &a, &b, &c);
+	if (a > b) {
+
+		if (a > c) {
+
+			if (b > c) printf("%d", b);
+
+			else printf("%d", c);
+		}
+
+		else printf("%d", a);
+	}
+
+	else {								//a<=b °¡Á¤
+
+		if (b > c) {
+			
+			if (a > c) printf("%d", a);
+
+			else printf("%d", c);
+		}
+
+		else printf("%d", b);
+
+	}
+
+	
+
+return 0;
+}
+
