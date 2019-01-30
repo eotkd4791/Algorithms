ca53971b7b156ac5d7f242c1da491c8de51bca17
eotkd4791 <eotkd4791@gmail.com>
2019-01-30 12:14:26 +0900

    190130

---
+++ LIS/BJ1965.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int dp[1001], arr[1001] = {};
+	int n, MAX = 0;
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	if (n == 1) {
+		printf("1\n");
+		return 0;
+	}
+	dp[0] = 1;
+	for (int i = 1; i < n; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j] && dp[j] + 1 > dp[i])
+				dp[i] = dp[j] + 1;
+			
+		}
+		if (MAX < dp[i]) MAX = dp[i];
+	}
+	printf("%d\n", MAX);
+	return 0;
+}
