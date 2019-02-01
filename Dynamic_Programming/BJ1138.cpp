a0a2b2c0e97a04c50de05c4dda523dc2e4b1c74b
eotkd4791 <eotkd4791@gmail.com>
2019-02-01 12:12:43 +0900

    190201

---
+++ Dynamic_Programming/BJ1138.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <cstring>
+using namespace std;
+vector<vector<int>> v(1005);
+queue<int> q;
+int N, M, V, a, b, visited[1005];
+
+void DFS (int now) {
+	visited[now] = 1;
+	printf("%d ", now);
+	for (int i = 0; i < v[now].size(); i++) {
+		int next = v[now][i];
+		if (visited[i] == 0) DFS(next);
+	}
+}
+
+void BFS(int start) {
+	visited[start] = 1;
+	q.push(start);
+	while (q.empty() == 0) {
+		int now = q.front();
+		q.pop();
+		printf("%d ", now);
+		for (int i = 0; i < v[now].size(); i++) {
+			int next = v[now][i];
+			if (visited[i] == 0) {
+				visited[next] = 1;
+				q.push(next);
+			}
+		}
+	}
+}
+
+int main() {
+	scanf("%d%d%d", &N, &M, &V);
+	for (int i = 1; i <= M; i++) {
+		scanf("%d%d", &a, &b);
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+	for (int i = 1; i <= N; i++) {
+		sort(v[i].begin(), v[i].end());
+	}
+	DFS(V);
+	printf("\n");
+	memset(visited, 0, sizeof(visited));
+	BFS(V);
+	return 0;
+}
---
+++ Theory/Binary_Search.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int Binary_Search(int arr[], int length, int target) { //배열 이름, 길이, 찾고자 하는 값
+	int first = 0;				//탐색 시작 인덱스 값
+	int last = length - 1;	//탐색 마지막 인덱스 값
+	int mid;						//탐색의 기준값, 한번 탐색후에 범위 재설정을 위한 변수
+
+	while (first <= last) {
+		mid = (first + last) / 2;
+		if (target == arr[mid]) return mid;	
+		else {
+			if (target < arr[mid]) last = mid - 1;
+			else first = mid + 1;
+		}
+	}
+	return -1;					 //탐색실패-찾는 값이 범위 내에 없을 때 -1반환
+}
+int main() {
+	int arr[] = { 1,3,5,7,9 };
+	int idx=0;
+	printf("%d ", arr[]);
+	idx = Binary_Search(arr, sizeof(arr) / sizeof(int), n);
+	if (idx == -1) printf("찾는 값이 범위 내에 없습니다.\n");
+	else printf("타겟이 저장되어 있는 인덱스는 %d 입니다.\n", idx);
+	
+	idx = Binary_Search(arr, sizeof(arr)/sizeof(int), 4);
+	if (idx == -1) printf("찾는 값이 범위 내에 없습니다.\n");
+	else printf("타겟이 저장되어 있는 인덱스는 %d 입니다.\n", idx);
+	return 0;
+}//이진탐색은 오름차순으로 정렬이 되어있는 상태에서 해야한다.
