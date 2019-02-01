6b11faa7a7b9ce5ef44e6453f789c0db409f00a9
eotkd4791 <eotkd4791@gmail.com>
2019-02-01 14:10:08 +0900

    190201

---
+++ Brute_Force/BJ14501.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int N;
+int T[20];
+int P[20];
+int MAX = 0;
+
+void money_max(int day, int pay) {
+	MAX = max(MAX, pay);
+	if (day == N + 1) return;
+	if (day + T[day] <= N + 1) {
+		money_max(day + T[day], pay + P[day]);
+	}
+	if (day + 1 <= N + 1) {
+		money_max(day + 1, pay);
+	}
+}
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 1; i <= N; i++) {
+		scanf("%d%d", &T[i], &P[i]);
+	}
+	money_max(1, 0);
+	printf("%d", MAX);
+	return 0;
+}
