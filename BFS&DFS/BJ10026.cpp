d4cdcdd6d45df140adfef318bd7ca7d0efe04b94
eotkd4791 <eotkd4791@gmail.com>
2019-02-01 17:31:18 +0900

    190201

---
+++ BFS&DFS/BJ10026.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int N;
+char field[101][101];
+int cnt;
+int check[101][101];
+int dr[4] = { 0,0,1,-1 };
+int dc[4] = { -1,1,0,0 };
+
+void DFS(int x, int y) {
+	if (check[x][y] != 0) return;
+	check[x][y] = 1;
+	for (int n = 0; n < 4; n++) {
+		int rx = x + dr[n];
+		int cy = y + dc[n];
+		if (rx >= 0 && cy >= 0 && rx < N && cy < N && field[rx][cy] == field[x][y] && check[rx][cy] == 0) {
+			DFS(rx, cy);
+		}
+	}
+}
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%s", &field[i]);
+	}
+	for (int j = 0; j < N; j++) {
+		for (int i = 0; i < N; i++) {
+			if (check[i][j]==0) cnt++;
+			DFS(i, j);
+		}
+	}
+	printf("%d ",cnt);
+	cnt = 0;
+	memset(check, 0, sizeof(check));
+	for (int j = 0; j < N; j++) {
+		for (int i = 0; i < N; i++)
+			if (field[i][j] == 'G') field[i][j] = 'R';
+	}
+	for (int j = 0; j < N; j++) {
+		for (int i = 0; i < N; i++) {
+			if (check[i][j]==0) cnt++;
+			DFS(i, j);
+		}
+	}
+	printf("%d\n", cnt);
+	return 0;
+}
---
+++ BFS&DFS/BJ11724.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+vector<vector<int>> cn(1005);
+queue<int>q;
+int N, M, u, v, check[1005], cnt;
+
+void BFS(int start) {
+	if (check[start] != 0) return;
+	check[start] = 1;
+	q.push(start);
+	while (q.empty() == 0) {
+		int now = q.front();
+		q.pop();
+		for (int i = 0; i < cn[now].size(); i++) {
+			int next = cn[now][i];
+			if (check[next] == 0) {
+				check[next] = 1;
+				q.push(next);
+			}
+		}
+	}
+}
+
+int main() {
+	scanf("%d%d", &N, &M);
+	for (int i = 0; i < M; i++) {
+		scanf("%d%d", &u, &v);
+		cn[u].push_back(v);
+		cn[v].push_back(u);
+	}
+	for (int i = 0; i < N; i++) {
+		sort(cn[i].begin(), cn[i].end());
+	}
+	for (int i = 1; i <= N; i++) {
+		if (check[i] == 0) cnt++;
+		BFS(i);
+	}
+	printf("%d\n", cnt);
+	return 0;
+}
