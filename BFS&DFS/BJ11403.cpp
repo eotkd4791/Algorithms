8607e30f7ec2b127257d5d4e87955cb372762439
Daesang You (Stephen) <46208349+eotkd4791@users.noreply.github.com>
2019-02-04 01:11:05 +0900

    Update BJ11403.cpp

--- BFS&DFS/BJ11403.cpp
+++ BFS&DFS/BJ11403.cpp
@@ -1,3 +1,5 @@
+/*경로 찾기*/
+
 #include <iostream>
 #include <algorithm>
 #include <queue>
@@ -8,14 +10,14 @@ vector<vector<int>>v(110);
 int N,arr[110][110],check[110];
 
 int BFS(int start, int target) {
-    queue<int>q={};
+    queue<int>q={};                     //큐 초기화 까먹지 않기 
     check[start]=1;
     q.push(start);
     while(q.empty()==0) {
         int now=q.front();
         q.pop();
         for(int k=0; k<v[now].size(); k++) {
-            if(v[now][k]==target) return 1;
+            if(v[now][k]==target) return 1; //이 부분 잘 기억하기
             int next=v[now][k];
             if(check[next]==0) {
                 check[next]=1;
