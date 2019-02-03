3dde2da608250a9435f83e649bbd007467dc5bbf
eotkd4791 <eotkd4791@gmail.com>
2019-02-04 01:08:34 +0900

    190203

--- Dynamic_Programming/BJ1932.cpp
+++ Dynamic_Programming/BJ1932.cpp
@@ -2,22 +2,22 @@
 #include <algorithm>
 using namespace std;
 
-int dp[510][510],arr[510][510];
+int n, dp[510][510], arr[510][510];
 
 int tri(int i, int j) {
 	if (i == n) return arr[i][j];
-	int &ret = dp[510][510];
+	int &ret = dp[i][j];
 	if (ret != 0) return ret;
-	ret = arr[i][j] + max(tri(i + 1, j) + (tri(i + 1, j + 1));
+	ret = arr[i][j] + max(tri(i + 1, j), tri(i + 1, j + 1));
 	return ret;
 }
 int main() {
-	int n = 0;
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= i; j++) {
 			scanf("%d", &arr[i][j]);
 		}
 	}
+	printf("%d\n", tri(0, 0));
 	return 0;
 }
