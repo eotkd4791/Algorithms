a787f824019035a19b5e3e8dacab7f3338058794
eotkd4791 <eotkd4791@gmail.com>
2019-02-21 14:59:22 +0900

    190221

---
+++ Dynamic_Programming/BJ1010.cpp
@@ -0,0 +1,31 @@
+//-다리놓기-
+//N<=M 조합nCk에서 n=M,k=N
+//조합공식을 nPk/k!으로 구현했지만, 시간초과가 났다.
+//그래서 이항계수 공식을 이용한 DP로 구현하였다.
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+int T, N, M;
+long long dp[30][30];
+
+long long cmb(int n, int k) {
+	long long &ret = dp[n][k];
+	if (k == 1) return n;
+	if (n == k) return 1;
+	if (k == 0) return 1;
+	if (ret > 0) return ret;
+	ret = 0;
+	ret = cmb(n - 1, k - 1) + cmb(n - 1, k);
+	return ret;
+}
+
+int main() {
+	memset(dp, -1, sizeof(dp));
+	scanf("%d", &T);
+	for (int i = 1; i <= T; i++) {
+		scanf("%d%d", &N, &M);
+		printf("%lld\n", cmb(M,N));
+	}
+	return 0;
+}
