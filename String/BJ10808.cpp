42ec25d9941294cc034b4d7eb7e3415938d2f331
eotkd4791 <eotkd4791@gmail.com>
2019-03-04 18:25:34 +0900

    190304

---
+++ String/BJ10808.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+char word[101];
+int alp[27];
+
+int main() {
+	scanf("%s", word);
+	int len = strlen(word);
+
+	for (int i = 0; i < len; i++) {
+		alp[word[i] - 'a']++;
+	}
+	for (int i = 0; i < 26; i++) {
+		printf("%d ",alp[i]);
+	}
+	return 0;
+}
