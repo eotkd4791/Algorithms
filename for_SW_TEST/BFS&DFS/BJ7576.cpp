031cece6cbb10e627806b891326a5c4ae198d532
eotkd4791 <eotkd4791@gmail.com>
2019-03-11 16:12:46 +0900

    [BOJ7576] 토마토

--- for_SW_TEST/BFS&DFS/BJ7576.cpp
+++ for_SW_TEST/BFS&DFS/BJ7576.cpp
@@ -2,11 +2,10 @@
 #include <algorithm>
 #include <cstring>
 #include <queue>
-#define INF 99999
 using namespace std;
 
 int N, M, ground[1005][1005], growth[1005][1005];
-int MIN = INF, day;
+int day;
 queue<pair<int, int> > q;
 int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
@@ -25,7 +24,7 @@ void BFS() {
 		for (int n = 0; n < 4; n++) {
 			int xafter = xtoday + dx[n];
 			int yafter = ytoday + dy[n];
-			
+
 			if (xafter >= 0 && yafter >= 0 && xafter < N && yafter < M && ground[xafter][yafter] == 0 && growth[xafter][yafter] == 0) {
 				growth[xafter][yafter] = day + 1;
 				ground[xafter][yafter] = ground[xtoday][ytoday];
@@ -40,27 +39,15 @@ int main() {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			scanf("%d", &ground[i][j]);
-		}
-	}
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			for (int k = 0; k < 4; k++) {
-				int xnow = i + dx[k];
-				int ynow = j + dy[k];
-				if (ground[i][j] > 0 && ground[xnow][ynow] == 0) {
-					q.push(make_pair(i, j));
-					break;
-				}
+			if (ground[i][j] == 1) {
+				q.push(make_pair(i, j));
 			}
 		}
 	}
 	BFS();
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			if (ground[i][j] == 0) {
-				printf("-1\n");
-				return 0;
-			}
+			if (ground[i][j] == 0) day = -1;
 		}
 	}
 	printf("%d\n", day);
