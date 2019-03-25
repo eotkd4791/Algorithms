dd1968b46dd439d289f36fdcb3c58dac12fba24e
eotkd4791 <eotkd4791@gmail.com>
2019-03-25 14:17:00 +0900

    [BOJ15651] N과M (3)

---
+++ BOJ/Backtracking/BJ15651.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+
+void recur(int *arr, int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			printf("%d ", arr[i] + 1);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+			arr[cnt] = i;
+			recur(arr, cnt + 1);
+	}
+}
+
+int main() {
+	scanf("%d%d", &N, &M);
+	int arr[10] = {};
+	bool check[10] = {};
+	recur(arr, 0);
+	return 0;
+}
