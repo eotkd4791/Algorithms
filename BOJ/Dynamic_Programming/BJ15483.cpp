f8a4ed7e2e2ecaf035097e3bbf6932adb7d5a5e5
stef <eotkd4791@gmail.com>
2019-06-12 17:25:45 +0900

    [BOJ15483] 최소 편집

---
+++ BOJ/Dynamic_Programming/BJ15483.cpp
@@ -0,0 +1,45 @@
+////////////////////////////////////
+/*
+       BOJ15483 최소 편집
+*/
+////////////////////////////////////
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#define INF 1005
+using namespace std;
+
+char str1[INF];
+char str2[INF];
+int dp[INF][INF];
+int n, m;
+
+int issame(int i, int j) {
+	if (str1[i-1] == str2[j-1])
+		return 0;
+	return 1;
+}
+
+int main() {
+	
+	scanf("%s", str1);
+	scanf("%s", str2);
+	n = strlen(str1);
+	m = strlen(str2);
+
+	memset(dp, -1, sizeof(dp));
+
+	dp[0][0] = 0;
+	for (int i = 1; i <= n; i++)
+		dp[i][0] = i;
+	for (int j = 1; j <= m; j++) 
+		dp[0][j] = j;
+	
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			if (dp[i][j] == -1)
+				dp[i][j] = min(dp[i - 1][j - 1] + issame(i, j), min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+	
+	printf("%d", dp[n][m]);
+	return 0;
+}
