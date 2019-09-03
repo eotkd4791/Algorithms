8c0f1532e2cf8bf7e9e3b64a2470c4534777ce0a
stef <eotkd4791@gmail.com>
2019-09-03 19:24:30 +0900

    [BOJ3190] 뱀

--- BOJ/Simulation/BJ3190.cpp
+++ BOJ/Simulation/BJ3190.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <deque>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
 int N, K, M;
-int field[101][101];//»ç°ú 1
+int field[101][101];
+bool check[101][101];
+typedef struct { int idx; char dir; }node;
 deque<pair<int, int> > D;
-char dir[10001];
+queue<node> q;
 int dx[] = { -1,0,1,0 };
 int dy[] = { 0,1,0,-1 };
 
@@ -29,18 +32,18 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> N;
-	cin >> K;
+	cin >> N >> K;
 	int a, b;
-	for (int k = 0; k < K; k++) {
+	while(K--) {
 		cin >> a >> b;
 		field[a][b] = 1;
 	}
+
 	cin >> M; 
 	int x; char d;
-	for (int l = 0; l < M; l++) {
+	while(M--) {
 		cin >> x >> d;
-		dir[x] = d;
+		q.push({ x,d });
 	}
 
 	D.push_back(make_pair(1, 1));
@@ -50,29 +53,29 @@ int main() {
 
 	while (1) {
 		time++;
+		check[nDx][nDy] = 1;
 		nDx += dx[idir];
 		nDy += dy[idir];
 
 		if (1 > nDx || nDx > N || N < nDy || nDy < 1)
 			break;
 
-	
-		for (int i = 0; i < D.size(); i++) 
-			if (nDx == D[i].first && nDy == D[i].second) {
-				cout << time;
-				return 0;
-			}
-		
-		D.push_front(make_pair(nDx, nDy));
+		if (check[nDx][nDy])
+			break;
 
-		if (field[nDx][nDy] == 1) 
+		D.push_front(make_pair(nDx, nDy));
+		if (field[nDx][nDy] == 1)
 			field[nDx][nDy] = 0;
-		
-		else if (field[nDx][nDy] == 0) 
+
+		else if (field[nDx][nDy] == 0) {
+			check[D.back().first][D.back().second] = 0;
 			D.pop_back();
+		}
 		
-		if (dir[time] != NULL)
-			idir = turn(idir,dir[time]);
+		if (q.front().idx == time) {
+			idir = turn(idir, q.front().dir);
+			q.pop();
+		}
 	}
 	cout << time;
 	return 0;
