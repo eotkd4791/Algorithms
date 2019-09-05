1662b531e392154d062643f0f79948c69a8f7cb8
stef <eotkd4791@gmail.com>
2019-09-05 17:18:10 +0900

    [BOJ11279] 최대 힙(구현)

--- BOJ/Heap/BJ11279.cpp
+++ BOJ/Heap/BJ11279.cpp
@@ -1,38 +1,86 @@
-/*
-=========================
-   BOJ11279 최대힙
-=========================
-*/
 #include <iostream>
-#include <functional>
-#include <queue>
-#include <vector>
+#define INF 100000
 using namespace std;
 
-int N;
-priority_queue<int, vector<int>, less<int>> q;
-//MIN = greater, MAX = less #include <functional>
+int N, x;
+int num, idx = 1;
+int heap[INF];
 
 int main() {
-	scanf("%d", &N);
+	ios::sync_with_stdio(0);
+	cin.tie(0);	cout.tie(0);
+
+	cin >> N;
 	while (N--) {
-		int X = 0;
-		scanf("%d", &X);
-		if (X == 0) {
-			if (q.empty() == 1) {
-				printf("0\n");
+		cin >> x;
+		if (x == 0) {//Delete
+			cout << heap[1] << '\n';
+			if (heap[1] == 0)
 				continue;
-			}
-			else {
-				printf("%d\n", q.top());
-				q.pop();
-				continue;
+			heap[1] = heap[num]; 
+			heap[num--] = 0;
+
+			idx = 1;
+			while (1) {
+				if (idx >= num)
+					break;
+				int left = idx * 2;
+				int right = idx * 2 + 1;
+				
+				if (left > num) //Left(x), Right(x)
+					break;
+				
+				if (left <= num && num < right) {// Left(o), Right(x)
+					if (heap[idx] < heap[left]) {
+						swap(heap[idx], heap[left]);
+						idx = left;
+						continue;
+					}
+					else
+						break;
+				}
+				if (right <= num) {// Left(o), Right(o)
+					if (heap[idx] <= heap[left] && heap[idx] <= heap[right]) {//both of sub trees have bigger value their super tree
+						if (heap[left] >= heap[right]) {
+							swap(heap[idx], heap[left]);
+							idx = left;
+							continue;
+						}
+						else {
+							swap(heap[idx], heap[right]);
+							idx = right;
+							continue;
+						}
+					}
+					if (heap[idx] >= heap[left] && heap[idx] <= heap[right]) {//only right sub tree have bigger value its super tree
+						swap(heap[idx], heap[right]);
+						idx = right;
+						continue;
+					}
+					if (heap[idx] >= heap[right] && heap[idx] <= heap[left]) {//only left sub tree have bigger value its super tree
+						swap(heap[idx], heap[left]);
+						idx = left;
+						continue;
+					}
+					if (heap[idx] >= heap[left] && heap[idx] >= heap[right])//both of them have less value
+						break;
+				}
 			}
 		}
-		if (X > 0) {
-			q.push(X);
-			continue;
+		else {//Insert
+			heap[++num] = x;
+			idx = num;
+			if (num == 1)
+				continue;
+			while (idx > 1) {
+				if (heap[idx] > heap[idx / 2]) {
+					swap(heap[idx], heap[idx / 2]);
+					idx /= 2;
+				}
+				else
+					break;
+			}
 		}
 	}
 	return 0;
-}   
+}
