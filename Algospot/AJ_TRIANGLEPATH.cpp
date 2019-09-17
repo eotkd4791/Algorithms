5e141366bba76f5b910c939088f6462f7de4fd48
stef <eotkd4791@gmail.com>
2019-09-17 19:05:55 +0900

    [AOJ] TRIANGLEPATH

---
+++ Algospot/AJ_TRIANGLEPATH.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int C, N;
+int arr[101][101];
+int dp[101][101];
+
+int recur(int x, int y) {
+	if (x == N) return arr[x][y];
+
+	int& ret = dp[x][y];
+	if (ret != 0) return ret;
+
+	ret = arr[x][y] + max(recur(x + 1, y), recur(x + 1, y + 1));
+	return ret;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> C;
+	while (C--) {
+		memset(dp, 0, sizeof(dp));
+		cin >> N;
+
+		for (int i = 1; i <= N; i++) 
+			for (int j = 1; j <= i; j++) 
+				cin >> arr[i][j];
+		
+		cout << recur(0, 0) << '\n';
+	}
+	return 0;
+}
