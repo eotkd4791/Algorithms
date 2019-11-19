b3b228b8e18c73653f1de2b185ae891abe392dac
stef <eotkd4791@gmail.com>
2019-11-20 02:51:18 +0900

    [BOJ10844] 쉬운 계단수

--- BOJ/Dynamic_Programming/BJ10844.cpp
+++ BOJ/Dynamic_Programming/BJ10844.cpp
@@ -11,21 +11,21 @@ int main()
 	cin.tie(0); cout.tie(0);
 
 	cin >> N;
+
 	for (int j = 1; j <= 9; j++)
 		dp[1][j] = 1;
-
-	for (int i = 2; i <= N; i++)
+	for (int i = 2; i <= N; i++) 
 		for (int j = 0; j <= 9; j++)
 		{
 			if (j == 0)
-				dp[i][j] = dp[i - 1][1] % mod;
+				dp[i][j] = dp[i - 1][1];
 			else
-				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1] % mod) % mod;
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
 		}
 
 	int sum = 0;
 	for (int j = 0; j <= 9; j++)
 		sum = (sum + dp[N][j] % mod) % mod;
-	cout << sum;
+	cout << sum << '\n';
 	return 0;
 }
