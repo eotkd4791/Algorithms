fa960bb156f97e648d5a6bfb4c2bf1e352f18bdf
Daesang You (Stephen) <eotkd4791@gmail.com>
2019-11-28 22:10:04 +0900

    [BOJ5532] 방학 숙제

--- BOJ/Simulation/BJ5532.cpp
+++ BOJ/Simulation/BJ5532.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+
 int L, A, B, C, D;
 int a, b, l;
 
 int main() {
 	scanf("%d%d%d%d%d", &L, &A, &B, &C, &D);
-	if (A%C != 0) a = A / C + 1;
-	else a = A / C;
-	if (B%D != 0) b = B / D + 1;
-	else b = B / D;
+	
+	if (A % C != 0) 
+		a = A / C + 1;
+	else
+		a = A / C;
+	
+	if (B % D != 0)
+		b = B / D + 1;
+	else
+		b = B / D;
+	
 	l = L - max(a, b);
+	
 	printf("%d\n", l);
 	return 0;
-}
+} 
