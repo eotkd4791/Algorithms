9d150d2017711ccb000b4dce4813d466ce8a8a48
Daesang You (Stephen) <eotkd4791@gmail.com>
2019-11-29 22:56:32 +0900

    [BOJ13460] 구슬 탈출 2

--- for_SW_TEST/BFS&DFS/BJ13460.cpp
+++ for_SW_TEST/BFS&DFS/BJ13460.cpp
@@ -93,7 +93,7 @@ int bfs() {
 
 int main() {
 	ios::sync_with_stdio(0);
-	cin.tie(0);
+	cin.tie(0); cout.tie(0);
 
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
@@ -113,4 +113,4 @@ int main() {
 	int ret = bfs();
 	cout << ret;
 	return 0;
-}
+} 
