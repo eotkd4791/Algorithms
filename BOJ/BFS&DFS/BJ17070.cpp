5f2367c5640f8fe90d0b6a95f5baa0c2d8825768
Daesang You (Stephen) <eotkd4791@gmail.com>
2020-01-10 23:50:51 +0900

    Update BJ17070.cpp

--- BOJ/BFS&DFS/BJ17070.cpp
+++ BOJ/BFS&DFS/BJ17070.cpp
@@ -62,11 +62,9 @@ void BFS() {
 
 int main() {
 	scanf("%d", &N);
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
+	for (int i = 1; i <= N; i++) 
+		for (int j = 1; j <= N; j++) 
 			scanf("%d", &field[i][j]);
-		}
-	}
 	BFS();
 	printf("%d", cnt);
 	return 0;
