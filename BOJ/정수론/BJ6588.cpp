882d33d4eadd8e4d58359267827d5e2d12161c6e
eotkd4791 <eotkd4791@gmail.com>
2020-02-06 20:03:13 +0900

    [BOJ6588] 골드바흐의 추측

---
+++ BOJ/정수론/BJ6588.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#define INF 1000000
+using namespace std;
+
+int a;
+bool check[INF+1];
+
+typedef struct {
+    int aa;
+    int bb;
+    int cc;
+}ctn;
+const string fail = "Goldbach's conjecture is wrong.";
+
+void getPrime() {
+    check[1] = check[INF] = true;
+    
+    for(int i = 2; i <= sqrt(INF); i++) {
+        if(check[i]) continue;
+        for(int j=i+i; j<=INF;a j+=i)
+            check[j]=true;
+        
+    }
+    check[2] = true;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    getPrime();
+
+    while(1) {
+        cin >> a;
+
+        if(a==0) break;
+        
+        ctn ans = {0, 0, 0};
+        for(int i=3; i <= a; i++) {
+            if(check[i]) continue;
+
+            int b = i;
+            int c = a - b;
+            if(!check[c]) {
+                if(b > c) swap(b,c);
+                if((ans.cc - ans.bb) <= c - b) ans = {a, b, c};
+                break;
+            }
+        }
+        if(ans.aa > 0 && ans.bb > 0 && ans.cc > 0)
+            cout << ans.aa << " = " << ans.bb << " + " << ans.cc << '\n';
+
+        else
+            cout << fail << '\n';
+    }
+    return 0;
+}
