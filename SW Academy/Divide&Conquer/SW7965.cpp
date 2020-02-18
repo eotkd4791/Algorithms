6e0ddcb67c82f622e60675fad1cad117aa4cc9bc
eotkd4791 <eotkd4791@gmail.com>
2020-02-18 22:07:41 +0900

    [SWEA7965] 퀴즈

--- SW Academy/Divide&Conquer/SW7965.cpp
+++ SW Academy/Divide&Conquer/SW7965.cpp
@@ -1,38 +1,41 @@
 #include <iostream>
 #define MOD 1000000007
 #define INF 1000000
+
 using namespace std;
- 
 typedef long long ll;
-ll p[INF + 1], sum[INF + 1];
-bool check[INF + 1];
- 
-ll power(ll mit, ll jisu) {
-    if(mit == 1) return 1;
+
+int T, N;
+ll sum[INF + 1];
+
+ll power(ll mit, ll jisu)
+{
     if(jisu == 0) return 1;
     if(jisu == 1) return mit;
-    if(check[mit]) return p[mit];
-     
+    
     ll root = power(mit, jisu / 2) % MOD;
-     
-    p[mit] = ((root % MOD) * (root % MOD)) % MOD;
-    check[mit] = true;
-     
-    if(jisu % 2 == 0) return p[mit];
-    else return (p[mit] * mit) % MOD;
+    
+    if(jisu % 2 == 1) return (root * ((root * mit) % MOD)) % MOD;
+    
+    else return (root * root) % MOD;
 }
- 
-int main() {
+
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-     
-    for(ll i = 1; i <= INF; i++) sum[i] = (sum[i - 1] % MOD + power(i, i) % MOD) % MOD;
-     
-    int T, N;
+    
+    sum[1] = 1; sum[2] = 5;
+    for(ll i = 3; i <= INF; i++)
+    {
+        sum[i] = (sum[i - 1] + power(i, i)) % MOD;
+    }
+    
     cin >> T;
-    for(int t = 1; t <= T; t++) {
+    
+    for(int t = 1; t <= T; t++)
+    {
         cin >> N;
-         
         cout << '#' << t << ' ' << sum[N] % MOD << '\n';
     }
     return 0;
