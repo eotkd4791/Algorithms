a9c964432a3eda1c5408d4d63dda7b55acba3ba9
eotkd4791 <eotkd4791@gmail.com>
2020-02-19 04:51:38 +0900

    [BOJ2630] 색종이 만들기

--- SW Academy/Divide&Conquer/BJ2630.cpp
+++ SW Academy/Divide&Conquer/BJ2630.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int N, white, blue;
@@ -7,65 +6,73 @@ int field[129][129];
 
 void recur(int sx, int sy, int fx, int fy)
 {
-    if(sx == fx || sy == fy) return;
-    
-    if( (sx + fx) / 2 == 0 && (sy + fy) / 2 == 0)
-    {
-        if(field[sx][sy] == 0) white++;
-        
-        else blue++;
-        
-        return;
-    }
-    
-    int tmp = field[sx][sy];
-    
-    bool flag = true;
-    
-    for(int i = sx; i < fx; i++)
-    {
-        for(int j = sy; j < fy; j++)
-        {
-            if(tmp != field[i][j]) flag = false;
-        }
-    }
-    
-    if(flag)
-    {
-        if(tmp == 0) white++;
-        
-        else blue++;
-    }
-    
+  if (sx == fx || sy == fy)
+    return;
+
+  if ((sx + fx) / 2 == 0 && (sy + fy) / 2 == 0)
+  {
+    if (field[sx][sy] == 0)
+      white++;
+
     else
+      blue++;
+
+    return;
+  }
+
+  int tmp = field[sx][sy];
+
+  bool flag = true;
+
+  for (int i = sx; i < fx; i++)
+  {
+    for (int j = sy; j < fy; j++)
     {
-        recur(sx, sy, (sx + fx) / 2, (sy + fy) / 2);
-        
-        recur(sx, (sy+fy) / 2, (sx + fx) / 2, fy);
-        
-        recur((sx+fx)/2, sy, fx, (sy + fy) / 2);
-        
-        recur((sx+fx)/2, (sy + fy)/2, fx, fy);
+      if (tmp != field[i][j])
+        flag = false;
     }
+  }
+
+  if (flag)
+  {
+    if (tmp == 0)
+      white++;
+
+    else
+      blue++;
+  }
+
+  else
+  {
+    recur(sx, sy, (sx + fx) / 2, (sy + fy) / 2); //좌상
+
+    recur(sx, (sy + fy) / 2, (sx + fx) / 2, fy); //우상
+
+    recur((sx + fx) / 2, sy, fx, (sy + fy) / 2); //좌하
+
+    recur((sx + fx) / 2, (sy + fy) / 2, fx, fy); //우하
+  }
 }
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    
-    cin >> N;
-    
-    for(int i = 0; i < N; i++)
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  cin >> N;
+
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < N; j++)
     {
-        for(int j = 0; j < N; j++)
-        {
-            cin >> field[i][j];
-        }
+      cin >> field[i][j];
     }
-    
-    recur(0, 0, N, N);
-    
-    cout << white << '\n' << blue;
-    return 0;
+  }
+
+  recur(0, 0, N, N);
+
+  cout << white << '\n'
+       << blue;
+  return 0;
 }
