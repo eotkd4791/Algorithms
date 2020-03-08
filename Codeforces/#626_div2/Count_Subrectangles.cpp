1d790e6f9398d4bce04c6245af87b4aa23175b98
Daesang You (Stephen) <eotkd4791@gmail.com>
2020-03-08 21:38:30 +0900

    #626_div2

--- Codeforces/#626_div2/Count_Subrectangles.cpp
+++ Codeforces/#626_div2/Count_Subrectangles.cpp
@@ -5,11 +5,9 @@ typedef long long ll;
 
 int main()
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+  ios::sync_with_stdio(0); cin.tie(0);
 
-  int n = 0, m = 0, k;
-  vector<ll> v;
+  int n = 0, m = 0, k; vector<ll> v;
 
   int a[40001] = {}, b[40001] = {};
 
@@ -17,22 +15,17 @@ int main()
 
   int MAX = n > m ? n : m;
 
-  for (int i = 0; i < n; i++)
-    cin >> a[i];
+  for (int i = 0; i < n; i++) cin >> a[i];
 
-  for (int j = 0; j < m; j++)
-    cin >> b[j];
+  for (int j = 0; j < m; j++) cin >> b[j];
 
   for (int i = 1; i <= k; i++)
   {
-    if (i > MAX)
-      break;
+    if (i > MAX) break;
 
-    if (k / i > MAX)
-      continue;
+    if (k / i > MAX) continue;
 
-    if (k % i == 0)
-      v.push_back(i);
+    if (k % i == 0) v.push_back(i);
   }
 
   ll sum = 0;
@@ -41,44 +34,36 @@ int main()
   {
     int left = h, right = (int)v.size() - 1 - h;
 
-    if (v[left] > n || v[right] > m)
-      continue;
+    if (v[left] > n || v[right] > m) continue;
 
     ll cnt = 0, nc = 0, mc = 0;
 
     for (int i = 0; i < n; i++)
     {
-      if (a[i] == 1)
-      {
-        cnt++;
-      }
+      if (a[i] == 1) cnt++;
+      
       else
       {
-        if (cnt >= v[left])
-          nc += cnt - v[left] + 1;
+        if (cnt >= v[left]) nc += cnt - v[left] + 1;
 
         cnt = 0;
       }
     }
-    if (cnt >= v[left])
-      nc += cnt - v[left] + 1;
+    if (cnt >= v[left]) nc += cnt - v[left] + 1;
 
     cnt = 0;
 
     for (int j = 0; j < m; j++)
     {
-      if (b[j] == 1)
-        cnt++;
+      if (b[j] == 1) cnt++;
       else
       {
-        if (cnt >= v[right])
-          mc += cnt - v[right] + 1;
+        if (cnt >= v[right]) mc += cnt - v[right] + 1;
 
         cnt = 0;
       }
     }
-    if (cnt >= v[right])
-      mc += cnt - v[right] + 1;
+    if (cnt >= v[right]) mc += cnt - v[right] + 1;
 
     sum += nc * mc;
   }
