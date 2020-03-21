4b7a59e2d2e6d81f93e92ff7035ef4115fbebfd1
eotkd4791 <eotkd4791@gmail.com>
2020-03-21 23:46:05 +0900

    [SWEA5653] 줄기세포배양

--- SW Academy/BFS&DFS/SW5653.cpp
+++ SW Academy/BFS&DFS/SW5653.cpp
@@ -4,16 +4,15 @@ using namespace std;
 int dx[] = { 0,0,1,-1 };
 int dy[] = { 1,-1,0,0 };
 int field[700][700];
-int T, n, m, k, ans;
+int T, n, m, k;
 
-typedef struct { int x; int y; int time; } node;
+typedef struct { int x; int y; int age; } node;
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
 
     cin >> T;
     for (int t = 1; t <= T; t++) {
-        memset(field, 0, sizeof(field)); ans = 0;
         queue<node> q[11];
 
         cin >> n >> m >> k;
@@ -21,7 +20,7 @@ int main() {
         for (int i = 350; i < n + 350; i++) {
             for (int j = 350; j < m + 350; j++) {
                 cin >> field[i][j];
-                if (field[i][j]) q[field[i][j]].push({i, j, field[i][j] * 2});
+                if (field[i][j] > 0) q[field[i][j]].push({i, j, field[i][j] * 2});
             }
         }
 
@@ -31,29 +30,28 @@ int main() {
 
                 while(qz--) {
                     node oq = q[i].front();
-                    int x = oq.x;
-                    int y = oq.y;
-                    int time = oq.time;
                     q[i].pop();
 
-                    time--;
+                    oq.age--;
 
-                    if (field[x][y] > time) { 
+                    if (field[oq.x][oq.y] > oq.age) { //활성 상태가 되면서 번
                         for (int dir = 0; dir < 4; dir++) {
-                            int nx = x + dx[dir];
-                            int ny = y + dy[dir];
+                            int nx = oq.x + dx[dir];
+                            int ny = oq.y + dy[dir];
                             if (field[nx][ny] == 0) {
-                                field[nx][ny] = field[x][y];
+                                field[nx][ny] = field[oq.x][oq.y];
                                 q[i].push({ nx, ny, field[nx][ny] * 2 });
                             }
                         }
                     }
-                    if (time > 0) q[i].push({ x, y, time });
+                    if (oq.age > 0) q[i].push({ oq.x, oq.y, oq.age });
                 }
             }
         }
+        int ans=0;
         for (int i = 1; i < 11; i++) ans += (int) q[i].size();
         cout << '#' << t << ' ' << ans << '\n';
+        memset(field, 0, sizeof(field));
     }
     return 0;
 }
