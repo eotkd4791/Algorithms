903d9784bf08df75fcfbcd8c1ea11e2770ea72dc
Daesang You (Stephen) <eotkd4791@gmail.com>
2020-07-28 02:57:00 +0900

    Refactored 입국심사.cpp

--- Programmers/Binary_Search/입국심사.cpp
+++ Programmers/Binary_Search/입국심사.cpp
@@ -1,36 +1,26 @@
 #include <vector>
 #include <algorithm>
-#include <climits>
 #define ll long long
 using namespace std;
 
-ll bs(ll start, ll end) {
-    return (start + end) / 2;
-}
-
 ll solution(int n, vector<int> v) {
-    ll ans = LLONG_MAX,
-        MIN = *min_element(v.begin(), v.end()),
-        MAX = MIN * n;
+    sort(v.begin(), v.end());
+    ll ans = 0,
+        left = 1,
+        right = v[v.size()-1] * (ll) n;
     
-    ll left = MIN, right = MAX;
-    ll time = bs(left, right);
-    
-    while(left<=right) {
+    while(left <= right) {
+        ll time = (left + right) / 2;
         ll sum = 0;
-        for(int i=0; i<v.size(); i++) 
+        
+        for(int i = 0; i < v.size(); i++) 
             sum += time / v[i];
 
         if(sum >= n) {
-            if(ans == time) break;
-            ans = min(ans, time);
-            right = time;
-            time = bs(left, right);
+            ans = time;
+            right = time - 1;
         }
-        else {
-            left = time + 1;
-            time = bs(left, right);
-        }
+        else left = time + 1;
     }
     return ans;
 }
