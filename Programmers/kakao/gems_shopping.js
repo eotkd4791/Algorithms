2645c4b7b609146cf5d8671f268ba701a4d0c9b5
eotkd4791 <eotkd4791@gmail.com>
2020-12-01 02:19:48 +0900

    보석쇼핑

---
+++ Programmers/kakao/gems_shopping.js
@@ -0,0 +1,27 @@
+const solution = gems => {
+  const gemSet = new Set(gems);
+  const gemMap = new Map();
+  const gemArr = [];
+
+  gems.forEach((v, i) => {
+    gemMap.delete(v);
+    gemMap.set(v, i);
+    
+    if(gemMap.size === gemSet.size) {
+      gemArr.push([gemMap.values().next().value + 1, i + 1]);
+    }
+  });
+  
+  gemArr.sort((a, b) => {
+    if(a[1] - a[0] < b[1] - b[0]) {
+      return -1;
+    }
+    if(a[0] < b[0]) {
+      return -1;
+    }
+    return 1;
+  });
+  return gemArr[0];
+}
+
+console.log(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]));
