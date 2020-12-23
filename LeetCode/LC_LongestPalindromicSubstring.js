2680d6e0006bde43179e41b9ea87d82106598549
eotkd4791 <eotkd4791@gmail.com>
2020-12-23 22:18:32 +0900

    LC_LongestPalindromicSubstring.js

--- LeetCode/LC_LongestPalindromicSubstring.js
+++ LeetCode/LC_LongestPalindromicSubstring.js
@@ -1,16 +1,16 @@
 const longestPalindrome = s => {
-  let ans = '';
-  const dp = JSON.parse(JSON.stringify(new Array(s.length).fill(new Array(s.length).fill(false))));
-  
-  for(let i = s.length-1; i >= 0; i--) {
-      for (let j = i; j < s.length; j++) {
-          if (s.charAt(i) === s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])) {
-              dp[i][j] = true;
-          }
-          if (dp[i][j] && ans.length < j - i + 1) {
-              ans = s.substring(i, j + 1);
-          }
-      }
-  }
-  return ans;
+    let ans = '';
+    const dp = JSON.parse(JSON.stringify(new Array(s.length).fill(new Array(s.length).fill(false))));
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        for (let j = i; j < s.length; j++) {
+            if (s.charAt(i) === s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])) {
+                dp[i][j] = true;
+            }
+            if (dp[i][j] && ans.length < j - i + 1) {
+                ans = s.substring(i, j + 1);
+            }
+        }
+    }
+    return ans;
 };
