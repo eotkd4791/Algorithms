8fccdbf50235ba6a518046eb58a01b21a2f1e310
eotkd4791 <eotkd4791@gmail.com>
2020-12-23 23:23:50 +0900

    Refactor LC_ReverseInteger.js

--- LeetCode/LC_ReverseInteger.js
+++ LeetCode/LC_ReverseInteger.js
@@ -1,7 +1,17 @@
 const reverse = x => {
-  const reverseStr = (x < 0 ? (-x) : x).toString().split('').reverse().join('');
+  let isMinus = false;
+
+  if(x < 0) {
+    x *= -1;
+    isMinus = true;
+  }
+  
+  const reverseStr = x.toString().split('').reverse().join('');
   const reverseNum = Number(isMinus ? '-'.concat(reverseStr) : reverseStr);
 
-  return  (2 ** 31) - 1 < reverseNum || (-2) ** 31 > reverseNum ? 0 : reverseNum;
-};
+  if((2 ** 31) - 1 < reverseNum || (-2) ** 31 > reverseNum) {
+    return 0;
+  }
 
+  return reverseNum;
+};
