cc107efdc398e925bea9248dba20e785f3093ec0
eotkd4791 <eotkd4791@gmail.com>
2021-01-03 17:52:43 +0900

    소수_찾기

--- Programmers/String/소수_찾기.js
+++ Programmers/String/소수_찾기.js
@@ -3,21 +3,20 @@ const map = new Map();
 
 function isPrime(str) {
   const toNum = Number(str);
-  if(map.has(toNum.toString())) return false;
   if(toNum <= 1) return false;
+  if(map.has(toNum.toString())) return false;
 
   const sqrtToNum = Math.sqrt(toNum);
   
   for(let i=2; i<=sqrtToNum; i++) {
     if(toNum % i===0) return false;
   }
-  map.set(toNum.toString(), true)
+  map.set(toNum.toString(), true);
   return true;
 }
 
 function recur(str, cur, arr, check) {
   if(str.length >= 1) {
-    console.log(str)
     if(isPrime(str)) answer++;
   }
   for(let i=0; i<arr.length; i++) {
@@ -36,5 +35,3 @@ function solution(numbers) {
   
   return answer;
 }
-
-console.log(solution('011'))
