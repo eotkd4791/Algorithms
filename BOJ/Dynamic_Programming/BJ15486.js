2e448395bd25fb3d086f0ba0f922fe393e9a2793
eotkd4791 <eotkd4791@gmail.com>
2021-09-05 03:12:58 +0900

    퇴사 2

--- BOJ/Dynamic_Programming/BJ15486.js
+++ BOJ/Dynamic_Programming/BJ15486.js
@@ -1,33 +1,15 @@
-const fs = require("fs");
+const fs = require('fs');
+const [n, ...t] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const input = () => {
-  const [n, ...table] = fs
-    .readFileSync("/dev/stdin")
-    .toString()
-    .trim()
-    .split("\n");
-
+function solution() {
   const N = +n;
-  const T = new Array(N + 1).fill(0);
-  const P = new Array(N + 1).fill(0);
+  const table = t.map((pair) => pair.split(' ').map(Number));
+  const dp = new Array(N + 1).fill(0);
 
-  table.forEach((pair, i) => {
-    const [t, p] = pair.split(" ").map(Number);
-    T[i + 1] = t;
-    P[i + 1] = p;
-  });
-  return { N, T, P };
-};
-
-const solution = () => {
-  const { N, T, P } = input();
-  const dp = new Array(N + 2).fill(0);
-
-  for (let i = N; i >= 1; i--) {
-    dp[i] =
-      i + T[i] > N + 1 ? dp[i + 1] : Math.max(dp[i + 1], dp[i + T[i]] + P[i]);
+  for (let i = N - 1; i >= 0; i--) {
+    dp[i] = table[i][0] + i >= N + 1 ? dp[i + 1] : Math.max(dp[i + 1], table[i][1] + dp[i + table[i][0]]);
   }
-  return dp[1];
-};
+  return dp[0];
+}
 
 console.log(solution());
