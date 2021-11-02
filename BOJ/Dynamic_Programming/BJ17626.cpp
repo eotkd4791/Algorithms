03740445b74691b9697c6e7773268ce6c12b214e
eotkd4791 <eotkd4791@gmail.com>
2021-11-02 15:51:14 +0900

    Four Squares

---
+++ BOJ/Dynamic_Programming/BJ17626.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 50000;
+int N;
+int dp[MAX + 1];
+
+void FillSquares() {
+  memset(dp, -1, sizeof(dp));
+  for(int i=1; i<=sqrt(MAX); i++) {
+    if(i * i <= MAX) {
+      dp[i * i] = 1;
+    }
+  }
+}
+
+int recur(int n) {
+  if(n <= 1) {
+    return n;
+  }
+  if(dp[n] > 0) {
+    return dp[n];
+  }
+  for(int i=sqrt(n); i>=1; i--) {
+    int square = i * i;
+    int count = recur(n - square) + recur(square);
+
+    if(dp[n] == -1) {
+      dp[n] = count;
+    }
+    else {
+      dp[n] = min(dp[n], count);
+    }
+  }
+  return dp[n];
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N;
+
+  FillSquares();
+  cout << recur(N) << '\n';
+  return 0;
+}
