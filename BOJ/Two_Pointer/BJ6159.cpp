e3b500d6be7560593b809fa6ddfebe825d7e1514
eotkd4791 <eotkd4791@gmail.com>
2021-11-09 17:50:11 +0900

    Update 코스튬 파티

--- BOJ/Two_Pointer/BJ6159.cpp
+++ BOJ/Two_Pointer/BJ6159.cpp
@@ -3,30 +3,30 @@
 using namespace std;
 
 int N, M;
-int Answer;
 int Cows[20001];
+int Answer;
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
   cin >> N >> M;
   for(int i=0; i<N; i++) {
     cin >> Cows[i];
   }
 
   sort(Cows, Cows + N);
-  int Right = N - 1;
 
-  for(int i=0; i<N; i++) {
-    if(Cows[i] > M / 2) {
+  int Right = N - 1;
+  for(int Left=0; Left < N; Left++) {
+    if(Cows[Left] > M / 2) {
       break;
     }
-    while(Cows[i] + Cows[Right] > M) {
+    while(Cows[Left] + Cows[Right] > M) {
       Right--;
     }
-
-    Answer += Right - i;
+    Answer += Right - Left;
   }
   cout << Answer << '\n';
+  return 0;
 }
