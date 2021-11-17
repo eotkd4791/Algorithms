65f0348f6017487931bb7eb96bf4570923d9bb1c
eotkd4791 <eotkd4791@gmail.com>
2021-11-17 23:58:58 +0900

    단어 뒤집기

---
+++ BOJ/String/BJ9093.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int T;
+string Words;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  string FlushBuffer;
+  cin >> T;
+
+  getline(cin, FlushBuffer);
+
+  while(T--) {
+    getline(cin, Words);
+
+    int Start = 0;
+    for(int i=0; i<Words.size(); i++) {
+      if(Words[i] == ' ') {
+        reverse(Words.begin() + Start, Words.begin() + i);
+        Start = i + 1;
+      }
+    }
+    reverse(Words.begin() + Start, Words.end());
+    cout << Words << '\n';
+  }
+  return 0;
+}
