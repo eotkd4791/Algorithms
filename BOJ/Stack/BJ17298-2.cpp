f8e8caa2aa5bae83e99449a292eb52e1608b557e
eotkd4791 <eotkd4791@gmail.com>
2021-11-19 03:58:20 +0900

    오큰수

--- BOJ/Stack/BJ17298-2.cpp
+++ BOJ/Stack/BJ17298-2.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <stack>
-#include <cstring>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 const int MAX = 1000001;
 stack<int> S;
 int N;
 int A[MAX];
-int Answer[MAX];
 
 int main() {
   ios::sync_with_stdio(0);
@@ -18,7 +18,7 @@ int main() {
     cin >> A[i];
   }
 
-  memset(Answer, -1, sizeof(Answer));
+  vector<int> Answer(N, -1);
 
   for(int i=0; i<N; i++) {
     while(!S.empty() && A[i] > A[S.top()]) {
@@ -27,8 +27,8 @@ int main() {
     }
     S.push(i);
   }
-  for(int i=0; i<N; i++) {
-    cout << Answer[i] << ' ';
+  for(const auto& element : Answer) {
+    cout << element << ' ';
   }
   return 0;
 }
