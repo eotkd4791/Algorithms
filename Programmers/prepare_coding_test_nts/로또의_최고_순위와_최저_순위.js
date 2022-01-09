30d331bb6a87d566e8b118f3dfa2fed4a39c398a
eotkd4791 <eotkd4791@gmail.com>
2022-01-10 01:46:54 +0900

    로또의 최고 순위와 최저 순위

---
+++ Programmers/prepare_coding_test_nts/로또의_최고_순위와_최저_순위.js
@@ -0,0 +1,31 @@
+function solution(lottos, winNums) {
+    let matchedCount = 0;
+    
+    for(const lotto of lottos) {
+        for(const winNum of winNums) {
+            if(lotto === winNum) {
+                matchedCount++;
+            }
+        }
+    }
+    
+    return [
+        getRank(matchedCount + getZeroCount(lottos)),
+        getRank(matchedCount),
+    ];
+}
+
+function getRank(matchingCount) {
+    switch(matchingCount) {
+        case 6: return 1;
+        case 5: return 2;
+        case 4: return 3;
+        case 3: return 4;
+        case 2: return 5;
+        default: return 6;
+    }
+}
+
+function getZeroCount(lottos) {
+    return lottos.filter(lotto => lotto === 0).length;
+}
