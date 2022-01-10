7194c5c0f537e5249d9645f8ad9eb8bffa6d25db
eotkd4791 <eotkd4791@gmail.com>
2022-01-10 23:35:35 +0900

    짝지어 제거하기

---
+++ Programmers/prepare_coding_test_nts/짝지어_제거하기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    const stack = [];
+    
+    for(let i=0; i<s.length; i++) {
+        if(stack.length > 0 && stack[stack.length - 1] === s[i]) {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+    return Number(stack.length === 0);
+}
