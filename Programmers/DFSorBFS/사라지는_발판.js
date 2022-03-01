1d3d5c1ca573d8e5a8334ce932bfcca23ab26cbc
eotkd4791 <eotkd4791@gmail.com>
2022-03-01 15:35:54 +0900

    사라지는 발판

--- Programmers/DFSorBFS/사라지는_발판.js
+++ Programmers/DFSorBFS/사라지는_발판.js
@@ -1,5 +1,5 @@
 let n, m;
-let dir = [ [0, 1], [0, -1], [-1, 0], [1, 0] ];
+let dir = [ [0, 1], [0, -1], [1, 0], [-1, 0] ];
 
 function isInside(x, y) {
     return 0 <= x && x < n && 0 <= y && y < m;
@@ -17,34 +17,32 @@ function canMove(board, x, y) {
 }
 
 function recur(board, ax, ay, bx, by) {
-    if(!canMove(board, ax, ay)) {
+    if(board[ax][ay] === 0 || !canMove(board, ax, ay)) {
         return [false, 0];
     }
     
-    const result = [false, 0];
-    if(board[ax][ay] === 1) {
-        let max = 0;
-        let min = 987654321;
+    let min = 987654321;
+    let max = 0;
+    let result = [false, 0];
+    
+    for(let i=0; i<4; i++) {
+        let nx = ax + dir[i][0];
+        let ny = ay + dir[i][1];
         
-        for(let i=0; i<4; i++) {
-            let nx = ax + dir[i][0];
-            let ny = ay + dir[i][1];
+        if(isInside(nx, ny) && board[nx][ny] === 1) {
+            board[ax][ay] = 0;
+            const [lose, dist] = recur(board, bx, by, nx, ny);
+            board[ax][ay] = 1;
             
-            if(isInside(nx, ny) && board[nx][ny] === 1) {
-                board[ax][ay] = 0;
-                const [win, dist] = recur(board, bx, by, nx, ny);
-                board[ax][ay] = 1;
-                
-                if(!win) {
-                    result[0] = true;
-                    min = Math.min(min, dist);
-                } else if(!result[0]) {
-                    max = Math.max(max, dist);
-                }
-            }   
+            if(!lose) {
+                result[0] = true;
+                min = Math.min(min, dist);
+            } else if(!result[0]) {
+                max = Math.max(max, dist);
+            }
         }
-        result[1] = result[0] ? min + 1 : max + 1;
     }
+    result[1] = result[0] ? min + 1 : max + 1;
     return result;
 }
 
