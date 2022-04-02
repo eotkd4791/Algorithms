85c950fcc7e4f74aba55fd15834093f87b8811a5
eotkd4791 <eotkd4791@gmail.com>
2022-04-03 05:24:28 +0900

    일곱 난쟁이

--- impact/week1/BOJ2309.cpp
+++ impact/week1/BOJ2309.cpp
@@ -7,22 +7,17 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  for(int i=0; i<9; i++) {
+  for(int i = 0; i < 9; i++) {
     cin >> heights[i];
   }
 
   sort(heights, heights + 9);
   do {
     int sum = 0;
-    for(int i=0; i<7; i++) {
-      sum += heights[i];
-    }
-    if(sum == 100) {
-      for(int i=0; i<7; i++) {
-        cout << heights[i] << '\n';
-      }
-      break;
-    }
+    for(int i = 0; i < 7; i++) sum += heights[i];
+    if(sum == 100) break;
   } while(next_permutation(heights, heights + 9));
+
+  for(int i = 0; i < 7; i++) cout << heights[i] << '\n';
   return 0;
 }
