aac37d71ec3621a21d01a01cf7e4fe7ba4f85e9f
eotkd4791 <eotkd4791@gmail.com>
2022-04-04 18:25:15 +0900

    팰린드림인지 확인하기

--- impact/week1/BOJ10988.cpp
+++ impact/week1/BOJ10988.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string word;
+string word, rword;
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
   
   cin >> word;
+  rword = word;
 
-  string rword = word;
   reverse(rword.begin(), rword.end());
-
   cout << (word == rword) << '\n';
   return 0;
 }
