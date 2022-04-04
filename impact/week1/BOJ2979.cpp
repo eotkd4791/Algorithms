ba9ed299adbeab6d5c70d43fa1d52491da0ec8e5
eotkd4791 <eotkd4791@gmail.com>
2022-04-04 15:55:40 +0900

    트럭 주차

--- impact/week1/BOJ2979.cpp
+++ impact/week1/BOJ2979.cpp
@@ -1,27 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a, b, c[3], cnt = -1, sum;
-pair<int, int> t[3];
+int A, B, C, from, to, c[101], sum;
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  cin >> c[0] >> c[1] >> c[2];
+  cin >> A >> B >> C;
   for(int i = 0; i < 3; i++) {
-    cin >> a >> b; 
-    t[i] = {a, b};
+    cin >> from >> to;
+    for(int j = from; j < to; j++) c[j]++;
   }
 
-  sort(t, t + 3);
-
-  for(int ti = 0; ti <=100; ti++) {
-    for(int i = 0; i < 3; i++) {
-      if(ti == t[i].first) cnt++;
-      if(ti == t[i].second) cnt--;
-    }
-    if(cnt > -1) sum += c[cnt] * (cnt + 1);
+  for(int i = 0; i <=100; i++) {
+    if(c[i] == 1) sum += A;
+    else if(c[i] == 2) sum += 2 * B;
+    else if(c[i] == 3) sum += 3 * C;
   }
   cout << sum;
   return 0;
