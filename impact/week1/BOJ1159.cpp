cea67d85a12f7168eb7f9ab39c063adbfb060dfe
eotkd4791 <eotkd4791@gmail.com>
2022-04-04 21:11:29 +0900

    농구 경기

--- impact/week1/BOJ1159.cpp
+++ impact/week1/BOJ1159.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
-map<char, vector<string>> m;
+int N, cnt[26];
 string p, answer;
 
 int main() {
@@ -10,22 +9,13 @@ int main() {
   cin.tie(0);
 
   cin >> N;
-
   for(int i=0; i<N; i++) {
     cin >> p;
-    if(m.find(p[0]) == m.end()) m.insert({p[0], vector<string>(1, p)});
-    else m[p[0]].push_back(p);
+    cnt[p[0] - 'a']++;
   }
 
-  for(const auto& ps : m) {
-    int size = (int) ps.second.size();
-    if(size >= 5) answer += ps.first;
-  }
-
-  if((int) answer.size() > 0) {
-    sort(answer.begin(), answer.end());
-    cout << answer << '\n';
-  } 
+  for(int i=0; i<26; i++) if(cnt[i] >= 5) answer += i + 'a';
+  if(answer.size()) cout << answer << '\n';
   else cout << "PREDAJA" << '\n';
   return 0;
 }
