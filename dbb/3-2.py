a90030d63bbe544c5a31bbedf9b069b09cd051f6
eotkd4791 <eotkd4791@gmail.com>
2023-06-04 20:44:27 +0900

    dbb-3-2

---
+++ dbb/3-2.py
@@ -0,0 +1,11 @@
+n,m,k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+count = int(m / (k + 1)) * k + m % (k + 1)
+result = count * first + (m - count) * second
+
+print(result)
